eb42665ddad016ad626e66a4e25b8569f138c34a
Florian <45694132+flo-bit@users.noreply.github.com>
2023-06-08 00:22:47 +0200

    remove unnecessary files, add readme

---
+++ Readme.md
@@ -0,0 +1,5 @@
+## uber-noise
+
+advanced noise generation for the browser and node.js
+
+Work in progress.
--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mkdir -p dist
-tsc --build tsconfig-commonjs.json
-tsc --build tsconfig-esm.json
-echo '{"type": "module"}' > dist/esm/package.json
