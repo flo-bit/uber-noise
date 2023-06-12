4322a3f491b5b5a38a6a6872cce1c4357109fdea
Florian <45694132+flo-bit@users.noreply.github.com>
2023-06-12 02:53:26 +0200

    cleanup, use webpack for dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,64 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
 const path = require("path");
 
-// eslint-disable-next-line no-undef
-module.exports = {
-  mode: "production",
-  entry: "./dist/esm/uber-noise.js",
+const config = {
+  mode: "development",
+  devtool: "source-map",
+  entry: "./uber-noise.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+};
+
+const esmConfig = {
+  ...config,
   output: {
-    filename: "uber-noise.js",
+    filename: "uber-noise.module.js",
+
     // eslint-disable-next-line no-undef
     path: path.resolve(__dirname, "dist"),
+  },
+  target: ["web"],
+  experiments: {
+    outputModule: true,
+  },
+};
 
+const umdConfig = {
+  ...config,
+  output: {
+    filename: "uber-noise.js",
+
+    // eslint-disable-next-line no-undef
+    path: path.resolve(__dirname, "dist"),
     library: {
       name: "UberNoise",
       type: "umd",
       export: "default",
     },
   },
+  name: "browser",
+
+  devServer: {
+    static: ["./examples", "./dist"],
+    port: 8765,
+    liveReload: true,
+    watchFiles: ["./uber-noise.ts"],
+
+    headers: {
+      "Cache-Control": "no-cache, no-store, must-revalidate",
+    },
+  },
 };
+
+// eslint-disable-next-line no-undef
+module.exports = [esmConfig, umdConfig];
